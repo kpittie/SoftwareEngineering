52f753490507e874933f6055a8333d328ae891bd
harshsaxena <harsh_surbhi94@yahoo.co.in>
2016-04-15 21:56:23 +0530

    Client Register Problem Completed

--- WebApp/templates/client/fetch_modules.php
+++ WebApp/templates/client/fetch_modules.php
@@ -3,14 +3,14 @@
      $user = 'root';
      $pass = '';
 
-     mysqli_connect($host, $user, $pass);
+     mysql_connect($host, $user, $pass);
 
-     mysqli_select_db('cmt');
+     mysql_select_db('cmt');
 
 
      $pid = intval($_GET['q']);
-     $find=mysqli_query("select * from module where project_id=$pid");
-     while($row=mysqli_fetch_array($find))
+     $find=mysql_query("select * from module where project_id=$pid");
+     while($row=mysql_fetch_array($find))
      {
          echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
      }
